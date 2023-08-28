11d25172550fb523668b68d01b6416fb78c0879f
MathiasSchjoedt-Bavngaard <msb@iterator-it.dk>
2023-08-28 14:54:48 +0200

    added Libarary into our project folder

---
+++ Excersise/ExerciseBasic/projects/models/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of models
+ */
+
+export * from './lib/course';
+export * from './lib/models.component';
+export * from './lib/models.module';
+export * from './lib/models.service';
