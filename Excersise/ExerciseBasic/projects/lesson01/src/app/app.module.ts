5b21f38a367814eb76bfaf275a53d4c81c42b262
MathiasSchjoedt-Bavngaard <msb@iterator-it.dk>
2023-08-28 14:21:07 +0200

    Tilføjet nav og course

--- Excersise/ExerciseBasic/projects/lesson01/src/app/app.module.ts
+++ Excersise/ExerciseBasic/projects/lesson01/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CourseListItemComponent } from './course-list-item/course-list-item.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavigationBarComponent],
+  imports: [BrowserModule, CourseListItemComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
